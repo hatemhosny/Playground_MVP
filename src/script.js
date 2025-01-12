497dd3317bc92750fb5b5f46b1759b1384ea8cc7
Hatem Hosny <hatemhosny@gmail.com>
2025-01-12 20:20:46 +0200

    feat: add languages: MD, SCSS, TS

--- src/script.js
+++ src/script.js
@@ -1,12 +1,30 @@
 // @ts-nocheck
 
+//#region UI
 const markup = document.getElementById("markup");
 const style = document.getElementById("style");
 const script = document.getElementById("script");
 const editors = [markup, style, script];
 const iframe = document.querySelector("iframe");
 const labels = document.querySelectorAll("#editors label");
+const menus = document.querySelectorAll("#editors select");
 
+labels.forEach((label) => {
+  label.addEventListener("dblclick", () => {
+    if (label.parentElement.style.flex.startsWith("15")) {
+      label.parentElement.style.flex = "1";
+    } else {
+      labels.forEach((lb) => {
+        lb.parentElement.style.flex = "1";
+      });
+      label.parentElement.style.flex = "15";
+    }
+  });
+});
+
+//#endregion
+
+//#region import maps
 // https://regexr.com/8a2j7
 const importsPattern =
   /(import\s+?(?:(?:(?:[\w*\s{},\$]*)\s+from\s+?)|))((?:".*?")|(?:'.*?'))([\s]*?(?:;|$|))/g;
@@ -34,6 +52,109 @@ ${JSON.stringify(importmap, null, 2)}
 
 const hasImports = (code) => getImports(code).length > 0;
 
+//#endregion
+
+// name, title, longTitle, editorId, compile
+const languages = [
+  {
+    name: "html",
+    title: "HTML",
+    longTitle: "HTML",
+    editorId: "markup",
+    compiler: async () => (code) => code,
+  },
+  {
+    name: "markdown",
+    title: "MD",
+    longTitle: "Markdown",
+    editorId: "markup",
+    compiler: async () => {
+      const marked = await import("https://esm.sh/marked");
+      return (code) => marked.parse(code);
+    },
+  },
+  {
+    name: "css",
+    title: "CSS",
+    longTitle: "CSS",
+    editorId: "style",
+    compiler: async () => (code) => code,
+  },
+  {
+    name: "scss",
+    title: "SCSS",
+    longTitle: "SCSS",
+    editorId: "style",
+    compiler: async () => {
+      const sass = await import("https://esm.sh/sass");
+      return (code) => sass.compileString(code).css;
+    },
+  },
+  {
+    name: "javascript",
+    title: "JS",
+    longTitle: "JavaScript",
+    editorId: "script",
+    compiler: async () => (code) => code,
+  },
+  {
+    name: "typescript",
+    title: "TS",
+    longTitle: "TypeScript",
+    editorId: "script",
+    compiler: async () => {
+      const ts = await import("https://esm.sh/typescript");
+      return (code) => ts.transpile(code);
+    },
+  },
+];
+
+const compilers = {};
+
+const getCompiler = async (language) => {
+  if (!compilers[language]) {
+    compilers[language] = await languages
+      .find((l) => l.name === language)
+      .compiler();
+  }
+  return compilers[language];
+};
+
+const compile = async (language, code) => {
+  const compiler = await getCompiler(language);
+  return compiler(code);
+};
+
+const getEditorLanguage = (editorId) =>
+  [...menus].find((menu) => menu.dataset.editorid === editorId).value;
+
+const loadMenus = () => {
+  menus.forEach((menu) => {
+    languages
+      .filter((language) => language.editorId === menu.dataset.editorid)
+      .forEach((language, index) => {
+        const option = document.createElement("option");
+        option.value = language.name;
+        option.textContent = language.longTitle;
+        option.selected = index === 0;
+        menu.appendChild(option);
+      });
+
+    const showSelected = () => {
+      const selected = languages.find(
+        (language) => language.name === menu.value
+      );
+      menu.parentElement.querySelector("span").textContent = selected.title;
+    };
+
+    showSelected();
+    menu.addEventListener("change", () => {
+      showSelected();
+      run();
+    });
+  });
+};
+
 const getResult = ({ html, css, js }) => {
   const importmapScript = hasImports(js)
     ? createImportMapScript(createImportMap(getImports(js)))
@@ -55,29 +176,31 @@ const getResult = ({ html, css, js }) => {
   `;
 };
 
-editors.forEach((editor) => {
-  editor.addEventListener("input", () => {
-    const resultHTML = getResult({
-      html: markup.value,
-      css: style.value,
-      js: script.value,
-    });
-
-    // TODO: Change This
-    // big security issue
-    iframe.srcdoc = resultHTML;
+const run = async () => {
+  const resultHTML = getResult({
+    html: await compile(getEditorLanguage("markup"), markup.value),
+    css: await compile(getEditorLanguage("style"), style.value),
+    js: await compile(getEditorLanguage("script"), script.value),
   });
-});
 
-labels.forEach((label) => {
-  label.addEventListener("dblclick", () => {
-    if (label.parentElement.style.flex.startsWith("15")) {
-      label.parentElement.style.flex = "1";
-    } else {
-      labels.forEach((lb) => {
-        lb.parentElement.style.flex = "1";
-      });
-      label.parentElement.style.flex = "15";
+  // TODO: Change This
+  // big security issue
+  iframe.srcdoc = resultHTML;
+};
+
+const start = () => {
+  loadMenus();
+
+  editors.forEach((editor) => {
+    editor.addEventListener("input", run);
+  });
+
+  document.addEventListener("keydown", (e) => {
+    if (e.key === "Enter" && (e.ctrlKey || e.metaKey)) {
+      e.preventDefault();
+      run();
     }
   });
-});
+};
+
+start();
